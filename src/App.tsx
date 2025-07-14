c19ec1dcbf6b353bc74e46b1823bf8a83f680cc3
Risk-taker0501 <risk.taker2024@gmail.com>
2025-07-14 09:56:39 +0300

    changed some logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/CompanyRegistration';
 import { MantineProvider } from '@mantine/core';
 
 export default function App() {
